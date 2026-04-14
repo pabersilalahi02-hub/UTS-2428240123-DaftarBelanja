d3a55254ae913cf7c859f95fe6c94545c3721b43
vii <pabersilalahi02@gmail.com>
2026-04-14 12:00:41 +0700

    commit kedua

---
+++ scriptls.js
@@ -0,0 +1,54 @@
+let nama = document.getElementById("nama-barang");
+let jumlah = document.getElementById("jumlah");
+let keterangan = document.getElementById("keterangan");
+
+function simpan() {
+    console.log(nama.value)
+    console.log(jumlah.value)
+    console.log(keterangan.value);
+
+    if (localStorage.getItem("belanja") === null) {
+        //simpan array kosong 
+        localStorage.setItem("belanja", "[]")
+    }
+
+    //panggil local storage 
+    let data = JSON.parse(localStorage.getItem("belanja"))
+    console.log(data)
+
+    data.push({
+        nama: nama.value,
+        jumlah: jumlah.value,
+        keterangan: keterangan.value
+    })
+    console.log(data)
+
+    //simpan data terbaru ke dalam local storage
+    //konversi dari object menjadi string
+
+    localStorage.setItem("belanja", JSON.stringify(data))
+
+    //tampil 
+    tampil()
+
+}
+
+   function tampil() {
+        //panggil dulu local storage
+        let hasil = JSON.parse(localStorage.getItem("belanja"))
+
+        //clear
+        document.getElementById("list-belanja").innerHTML = ""
+
+        //lakukan perulangan (forEach)
+        hasil.forEach(element => {
+            //console.log(element)
+            document.getElementById("list-belanja").innerHTML += `<div class="col-lg-4 col-md-6 col-sm-12">
+            <h4 class="text-primary">${element.nama}</h4> 
+            <h6 class="text-danger">${element.jumlah}</h6>
+            <img src="${element.keterangan}" alt="${element.nama}" class="img-fluid mt-2">
+            <div> ` 
+
+        });
+    }
+
